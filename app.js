cacf98c59d336d82b0f378f04edf1af6bc33154c
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-02-21 01:06:44 -0500

    end level 1

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+//pp 2
+const express = require('express')
+const app = express()
+const port = 8080
 
+let = offersList = []
 
-console.log("Hola mundo")
+app.get('/', (req, res) => res.json(offersList))
+
+app.post('/', (req, res) => {
+    offersList.push(`Soy la oferta ${offersList.length + 1}`)
+    const response = { offers: offersList }
+    res.json( response )
+})
+
+app.put('/:offer_id', (req, res) => {
+    res.json(offersList.splice(req.params.offer_id, 1, `oferta editada ${req.params.offer_id}`))
+})
+
+app.delete('/:offer_id', (req, res) => {
+    offersList.splice(req.params.offer_id, 1)
+    res.json(offersList)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//pp 1
+//console.log("Hello World")
